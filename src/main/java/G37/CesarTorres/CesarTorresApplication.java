e03c52a0a8a6d05b66393c3d7a6a5e0286da6934
CesarTorresB <cesar.torres.mt@correo.usa.edu.co>
2021-10-19 12:17:22 -0500

    first commit

---
+++ src/main/java/G37/CesarTorres/CesarTorresApplication.java
@@ -0,0 +1,13 @@
+package G37.CesarTorres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesarTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesarTorresApplication.class, args);
+	}
+
+}
