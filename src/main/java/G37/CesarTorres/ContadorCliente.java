d2fef7276a54a47a411f680365b4e78815c41cdd
CesarTorresB <cesar.torres.mt@correo.usa.edu.co>
2021-10-30 19:46:00 -0500

    veinte commit

---
+++ src/main/java/G37/CesarTorres/ContadorCliente.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package G37.CesarTorres;
+
+/**
+ *
+ * @author cesartbossa
+ */
+public class ContadorCliente {
+    private long total;
+    private Cliente client;
+
+    public ContadorCliente(long total, Cliente client) {
+        this.total = total;
+        this.client = client;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public Cliente getClient() {
+        return client;
+    }
+
+    public void setClient(Cliente client) {
+        this.client = client;
+    }
+    
+}
--- src/main/java/G37/CesarTorres/RepositorioReservaciones.java
+++ src/main/java/G37/CesarTorres/RepositorioReservaciones.java
@@ -6,6 +6,8 @@ package G37.CesarTorres;
  */
 
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,4 +35,22 @@ public class RepositorioReservaciones {
     public void delete(Reservaciones reservation){
         crud4.delete(reservation);
     }
+    
+    public List<Reservaciones> ReservacionStatusRepositorio (String status){
+         return crud4.findAllByStatus(status);
+     }
+     
+     public List<Reservaciones> ReservacionTiempoRepositorio (Date a, Date b){
+         return crud4.findAllByStartDateAfterAndStartDateBefore(a, b);
+     
+     }
+     
+     public List<ContadorCliente> getClientesRepositorio(){
+         List<ContadorCliente> res = new ArrayList<>();
+         List<Object[]> report = crud4.countTotalReservationsByClient();
+         for(int i=0; i<report.size(); i++){
+             res.add(new ContadorCliente((Long)report.get(i)[1],(Cliente) report.get(i)[0]));
+         }
+         return res;
+     } 
 }
